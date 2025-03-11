6fab846b935a100460fdb84d8e79b29e0f1ee45f
Ceereals <romoliriccardo.rr@gmail.com>
2025-03-11 01:39:26 +0100

    feat: :sparkles: add abortable promise support to asyncCompose and update package.json dependencies

--- react-pdf/fns/src/asyncCompose.ts
+++ react-pdf/fns/src/asyncCompose.ts
@@ -1,41 +1,61 @@
 /* eslint-disable no-await-in-loop */
 
-type Fn = (arg: any, ...args: any[]) => Promise<any> | any;
+type Fn = (arg: any, ...args: any[]) => Promise<any> | any
 
 type FirstFnParameterType<T extends Fn[]> = T extends [
   ...any,
   (arg: infer A, ...args: any[]) => Promise<any> | any,
 ]
   ? A
-  : never;
+  : never
 
 type LastFnReturnType<T extends Fn[]> = T extends [
   (arg: any, ...args: any[]) => Promise<infer R> | infer R,
-  ...any,
+  ...any[],
 ]
   ? R
-  : never;
+  : never
+
+// Extend the Promise interface to include an abort method.
+interface AbortablePromise<T> extends Promise<T> {
+  abort: () => void
+}
+
 /**
  * Performs right-to-left function composition with async functions support
+ * and returns an abortable promise.
  *
- * @param fns - Functions
- * @returns Composed function
+ * @param fns - Functions to compose
+ * @returns A function that, when invoked, returns an AbortablePromise
  */
 const asyncCompose =
   <T extends Fn[]>(...fns: T) =>
-  async (
+  (
     value: FirstFnParameterType<T>,
     ...args: Parameters<T[0]> extends [any, ...infer Rest] ? Rest : []
-  ): Promise<LastFnReturnType<T>> => {
-    let result = value;
-    const reversedFns = fns.slice().reverse();
+  ): AbortablePromise<LastFnReturnType<T>> => {
+    let abort: () => void = () => {}
 
-    for (let i = 0; i < reversedFns.length; i += 1) {
-      const fn = reversedFns[i];
-      result = await fn(result, ...args);
-    }
+    // Create a promise that will never resolve normally, but can be rejected on abort.
+    const abortPromise = new Promise<never>((_, reject) => {
+      abort = () => reject(new Error('Async compose aborted'))
+    })
 
-    return result as LastFnReturnType<T>;
-  };
+    // Compose the functions. Each async step is raced against the abortPromise.
+    const composedPromise = (async () => {
+      let result = value
+      const reversedFns = fns.slice().reverse()
 
-export default asyncCompose;
+      for (const fn of reversedFns) {
+        result = await Promise.race([fn(result, ...args), abortPromise])
+      }
+
+      return result as LastFnReturnType<T>
+    })() as AbortablePromise<LastFnReturnType<T>>
+
+    // Attach the abort method to the promise.
+    composedPromise.abort = abort
+    return composedPromise
+  }
+
+export default asyncCompose
