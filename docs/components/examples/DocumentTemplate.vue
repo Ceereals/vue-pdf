1eb88dbed221378208f0b8c0b6357a6da0bc0c73
Ceereals <romoliriccardo.rr@gmail.com>
2025-03-26 01:15:11 +0100

    feat: :sparkles: add DocumentTemplate, LiveCode, and PageCounter components for PDF rendering

---
+++ docs/components/examples/DocumentTemplate.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Document, Page, Text } from '@/components'
+import PageCounter from './PageCounter.vue'
+defineProps<{
+  author?: string
+}>()
+</script>
+<template>
+  <Document>
+    <Page>
+      <Text>{{ author }}</Text>
+      <Text>Hello World!</Text>
+      <PageCounter />
+    </Page>
+    <Page>
+      <Text>Hello World 2!</Text>
+      <PageCounter />
+    </Page>
+  </Document>
+</template>
---
+++ docs/components/examples/PageCounter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { View, Text } from '@/components'
+</script>
+<template>
+  <View
+    :style="{
+      position: 'absolute',
+      bottom: 0,
+      textAlign: 'center',
+      width: '100%',
+    }"
+  >
+    <Text
+      :render="({ pageNumber, totalPages }) => `${pageNumber}/${totalPages}`"
+    />
+  </View>
+</template>
